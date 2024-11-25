5daf982ae3f916b484b56f7a84a1359269fbc2ce
Ivan Zazic <zazicivan@gmail.com>
2024-11-25 01:34:37 +0100

    [ADD] prototype of my_strlen function

--- includes/prototypes.h
+++ includes/prototypes.h
@@ -15,9 +15,12 @@
     #include "includes.h"
     #include "struct.h"
 
+// GUI
 Button_t createButton(sfFont *font, char *text, sfVector2f position, sfVector2f size);
 Struct_t *initGame(void);
 void handleEvents(Struct_t *cliz);
+void handleMouseClick(Struct_t *cliz, sfVector2f mousePosF);
+void handleTextEntered(Struct_t *cliz, sfEvent event);
 void drawHomePage(Struct_t *cliz);
 void drawWindow(Struct_t *cliz);
 void cleanup(Struct_t *cliz);
@@ -25,6 +28,13 @@ Animation_t *loadAnimation(char *filename, int frameCount, float frameDuration, 
 void destroyAnimation(Animation_t *animation);
 void playAnimation(sfRenderWindow *window, Animation_t *animation);
 void drawSignUpPage(Struct_t *cliz);
+void drawTextBox(sfRenderWindow *window, sfFont *font, char *label, sfVector2f position);
 void drawChatbotPage(Struct_t *cliz);
+bool loadFonts(Struct_t *cliz);
+bool loadTextures(Struct_t *cliz);
+void initChatbox(Struct_t *cliz);
+
+// lib
+int my_strlen(char const *str);
 
 #endif
